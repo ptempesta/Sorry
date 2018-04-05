2e23cc4bbea072f7f47989d172be7e9a8ec91767
ptempesta <ptempesta23@gmail.com>
2018-04-05 16:03:39 -0400

    The deck seems to shuffle and draw correctly. So far, the deck shuffles and draws cards which can in turn have methods called on them which display text. They don't really do anything yet, but it seems the framework is getting set up.

--- deck.py
+++ deck.py
@@ -10,11 +10,49 @@ import threeCard
 import twoCard
 import oneCard
 
+import random
+
 # This class will simulate a deck of cards for the Sorry! board game. This class
 # will be composed of a list of Sorry! board game cards. The list will consist
 # of 45 cards, 4 of each card excepting the oneCard, of which there will be 5.
 
-testCard = sorryCard.sorryCard
-testCard.printText()
-testCard.option1()
-testCard.option2()
+class deck:
+    cardStack = []
+    # The draw method will pop one card off from the end of the list, as if a
+    # card was drawn from the deck and discarded. However, if there are no cards
+    # left in the list, the list will be repopulated with cards and shuffled.
+    def draw(cardStack):
+        if len(cardStack) == 0:
+            for i in range(0,4):
+                cardStack.append(sorryCard.sorryCard)
+                cardStack.append(twelveCard.twelveCard)
+                cardStack.append(elevenCard.elevenCard)
+                cardStack.append(tenCard.tenCard)
+                cardStack.append(eightCard.eightCard)
+                cardStack.append(sevenCard.sevenCard)
+                cardStack.append(fiveCard.fiveCard)
+                cardStack.append(fourCard.fourCard)
+                cardStack.append(threeCard.threeCard)
+                cardStack.append(twoCard.twoCard)
+            for i in range(0,5):
+                cardStack.append(oneCard.oneCard)
+            random.shuffle(cardStack)
+            return cardStack.pop()
+        else:
+            return cardStack.pop()
+    
+            
+# The following code is meant to test this class.    
+#testCard = sorryCard.sorryCard
+#testCard.printText()
+
+#testDeck = deck
+#testDeck.cardStack[0].printText()
+#print(len(testDeck.cardStack))
+#testDeck.cardStack.pop().printText()
+#print(len(testDeck.cardStack))
+
+#testDeck = deck
+#print(len(testDeck.cardStack))
+#testDeck.draw(testDeck.cardStack).printText()
+#print(len(testDeck.cardStack))
