21c699b138fe7ed6be5efb625e85120eb4180f6e
ptempesta <ptempesta23@gmail.com>
2018-04-26 15:10:31 -0400

    Altered card text, added newlines.

--- card.py
+++ card.py
@@ -5,25 +5,26 @@
 # Peter Tempesta, 4/4/2018
 
 class sorryCard:
-    cardMessage = "Move a pawn from your start area to take the place of \
-another player's pawn which must return to its own start area OR move forward \
+    cardMessage = "Move a pawn from\nyour start area\nto take the place\nof \
+another\nplayer's pawn\nwhich must return\nto its own start\narea OR move\nforward \
 4."
 
 class twelveCard:
     cardMessage = "Move forward 12."
 
 class elevenCard:
-    cardMessage = "Move forward 11 OR change places with an opponent."
+    cardMessage = "Move a pawn from\nyour start area\nto take the place\nof \
+another\nplayer's pawn\nwhich must return\nto its own start\narea OR move\nforward \
+11."
 
 class tenCard:
-    cardMessage = "Move forward 10 OR move backward 1."
+    cardMessage = "Move forward 10\nOR move backward 1."
 
 class eightCard:
     cardMessage = "Move forward 8."
 
 class sevenCard:
-    cardMessage = "Move forward 7 OR split the move between two of \
-your pawns."
+    cardMessage = "Move forward 7."
 
 class fiveCard:
     cardMessage = "Move forward 5."
